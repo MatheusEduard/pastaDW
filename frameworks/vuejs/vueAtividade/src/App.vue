1dee494fdc6c7825e820b5c5cdd683c8dd996ca9
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2022-04-20 11:00:45 -0300

    Add files via upload

---
+++ frameworks/vuejs/vueAtividade/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhoTeste from "@/components/CabecalhoTeste.vue";
+import PrincipalTeste from "./components/PrincipalTeste.vue";
+import RodapeTeste from "./components/RodapeTeste.vue";
+export default {
+  components: { MenuLateral, CabecalhoTeste, PrincipalTeste, RodapeTeste },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabecalhoTeste />
+  <PrincipalTeste />
+  <RodapeTeste />
+  <footer id="footer">Rodapé</footer>
+</template>
+<style></style>
