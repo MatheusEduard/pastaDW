d1abbf8f54332a8c33bdb39c568111a346761b10
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2022-04-28 10:43:54 -0300

    Add files via upload

---
+++ exercicios/exercicioKambam/src/components/BoxBoard.vue
@@ -0,0 +1,15 @@
+<script>
+import ItensLista from "./ItensLista.vue";
+export default {
+    props: ["title","lista"],
+    components: { ItensLista }
+};
+</script>
+
+<template>
+  <div class="box">
+      <h2>{{ title }}</h2>
+      <ItensLista :lista = "lista"/>
+      </div>
+  
+</template>
