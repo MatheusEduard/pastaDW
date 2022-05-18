3efc15f572237a376b79f89b4a6428dc8090716e
MatheusEduard <matheuseduardo8000@gmail.com>
2022-05-18 11:50:50 -0300

    exercicioPinia

---
+++ exercicios/exercicioPinia/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script>
+import { mapStores, mapActions, mapState } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
+export default {
+  computed: {
+    ...mapStores(useCounterStore),
+    ...mapState(useCounterStore, "counter", "doubleCount"),
+  },
+
+  methods: {
+    ...mapActions(useCounterStore, ["increment"]),
+    adicionar() {
+      this.increment();
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <h1>Contador{{ counter }}</h1>
+  </main>
+</template>
